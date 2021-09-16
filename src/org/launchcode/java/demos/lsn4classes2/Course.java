2bed9a79a688f5a582a67e64da6d9632ae036639
Kroese <Josh.Kroese@fike.com>
2021-09-15 20:37:22 -0500

    completed classes and objects part 2 exercises

--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -1,11 +1,37 @@
 package org.launchcode.java.demos.lsn4classes2;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
     private String topic;
     private Teacher instructor;
     private ArrayList<Student> enrolledStudents;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return topic.equals(course.topic) && instructor.equals(course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topic, instructor);
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public Teacher getInstructor() {
+        return instructor;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
