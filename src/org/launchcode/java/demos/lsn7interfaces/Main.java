775dd26d6aa700e5ca44eed406b6d56109a97203
Kroese <Josh.Kroese@fike.com>
2021-09-28 15:17:52 -0500

    completed lesson 7 interfaces exercises

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
@@ -8,11 +9,35 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
+
+
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
         //  field.
+        for(Flavor i: flavors){
+            System.out.println(i.getName());
+        }
+        System.out.println();
+
+        Comparator comparator = new FlavorComparator();
+        flavors.sort(comparator);
+
+        for(Flavor i: flavors){
+            System.out.println(i.getName());
+        }
+        System.out.println();
+
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
         //  field.
+        for(Cone i: cones){
+            System.out.println(i.getName()+": "+i.getCost());
+        }
+        System.out.println();
+
+        cones.sort(new ConeComparator());
+        for(Cone i: cones){
+            System.out.println(i.getName()+": $"+i.getCost());
+        }
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
---
+++ src/org/launchcode/java/studios/funwithquizzes/RunQuiz.java
@@ -0,0 +1,2 @@
+package org.launchcode.java.studios.funwithquizzes;public class RunQuiz {
+}
